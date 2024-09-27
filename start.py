dec58523b3f690fc26eb37dfa373d5f3ee7f85e6
chandana-arch <yadavchandana06@gmail.com>
2024-09-27 16:16:08 +0530

    removed ervy

--- start.py
+++
@@ -1,9 +0,0 @@
-input=int(input('enter val'))
-if input%2==0:
-    print('even',input)
-else:
-    print('odd',input)
-def palin(input):
-    return input[::-1]==input
-def square(input):
-    return input**2
