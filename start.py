116e98427a966d71d471c7d314ac9cd2ee4e5229
chandana-arch <yadavchandana06@gmail.com>
2024-09-27 16:28:37 +0530

    modified hi fun

---
+++ start.py
@@ -0,0 +1,11 @@
+input=int(input('enter val'))
+if input%2==0:
+    print('even',input)
+else:
+    print('odd',input)
+    print('hooo')
+def palin(input):
+    return input[::-1]==input
+def square(input):
+    
+    return input**2
